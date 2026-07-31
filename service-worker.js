6675cbab5ba54cfa51d8f8b560217a8881edd971
Abhishek-p7 <153258892+Abhishek-p7@users.noreply.github.com>
2026-08-01 00:38:59 +0530

    Refresh app cache for new design

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,1 @@
-const CACHE_NAME="sonu-golu-protected-v2";
-const FILES=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg"];
-self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(FILES)));self.skipWaiting()});
-self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim()});
-self.addEventListener("fetch",event=>{event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request)))});
+const C="sonu-golu-v4",F=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./background.svg"];addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));skipWaiting()});addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));clients.claim()});addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
